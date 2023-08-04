f6e1f3fac0e4405bfb6222f021333c2c219cc25c
AndresCespedes <felipecespedes1999@hotmail.com>
2023-08-04 18:53:13 -0500

    refactor json, component(Products.jsx) and css

--- Components/Products.jsx
+++ Components/Products.jsx
@@ -1,6 +1,5 @@
-import { Prosto_One } from "next/font/google";
 import { useEffect, useState } from "react";
-import styles from '../src/styles/Products.module.css'
+import styles from "../src/styles/Products.module.css";
 
 export const Products = () => {
   const [products, setProducts] = useState([]);
@@ -16,41 +15,30 @@ export const Products = () => {
 
   return (
     <>
-      <div className={styles['product-list']}>
-        <h1>Lista de Productos</h1>
+      <h1 className={styles["title-products"]}>Lista de Productos</h1>
+      <div className={styles["product-list"]}>
         {products.map((product) => (
-          <div key={product.id} className={styles["product-container"]}>
-            <h2>{product.name}</h2>
-            <p>Categoría: {product.category}</p>
-            <p>Precio: ${product.price}</p>
+          <div key={product.book.ISBN} className={styles["product-container"]}>
+            <h2>{product.book.title}</h2>
             <p>
-              Disponibilidad:
-              {product.availability ? " Disponible" : " No disponible"}
+              {" "}
+              <b>Género: </b> {product.book.genre}
             </p>
-            <p className={product.availability ? "" : styles["product-not-avaliable"]}>
-              {product.description}
+            <p>
+              <b>Sinopsis: </b> {product.book.synopsis}
             </p>
-            {/* Aquí puedes mostrar las imágenes y reseñas si las tienes */}
-            {product.images && product.images.length > 0 && (
-              <div>
-                <h3>Imágenes del Producto:</h3>
-                {product.images.map((image, index) => (
-                  <img key={index} src={image} alt={`Imagen ${index + 1}`} />
-                ))}
-              </div>
-            )}
-            {product.reviews && product.reviews.length > 0 && (
-              <div>
-                <h3>Reseñas:</h3>
-                {product.reviews.map((review) => (
-                  <div key={review.id}>
-                    <p>{review.user}</p>
-                    <p>Rating: {review.rating}</p>
-                    <p>{review.comment}</p>
-                  </div>
-                ))}
-              </div>
-            )}
+            <p>
+              <b>Autor: </b> {product.book.author.name}
+            </p>
+            <p>
+              <b>Año: </b>
+              {product.book.year}
+            </p>
+            <p>
+              <b>Precio: </b>${product.book.price}
+            </p>
+            <h3>Portada del libro</h3>
+            <img src={product.book.cover} alt={`Imagen`} />
           </div>
         ))}
       </div>
